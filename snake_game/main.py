4e709a0bb40523dcc445eb4ca306c4bf8eaf4016
gdlhh <han_ger05@hotmail.com>
2022-09-22 01:56:10 +0800

    created the initialised state of snake and method for snake to move straight

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,6 +1,7 @@
-from re import X
-from turtle import Screen, Turtle, color
+from turtle import Screen
+from snake import Snake
 import time
+
 
 # initialise the window
 
@@ -8,32 +9,17 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake game")
-x_position = 0
-turtle_list = []
 screen.tracer(0)
 
-# initialising the snake
-for turtle in range(0,3):
-    x_position -= 20
-    new_turtle = Turtle()
-    new_turtle.penup()
-    new_turtle.color("white")
-    new_turtle.shape("square")
-    new_turtle.goto(x=x_position, y=0)
-    turtle_list.append(new_turtle)
+snake = Snake()
 
+game_is_on = True
 
-
-# moving the turtles forwards
-while True:
+while game_is_on:
     screen.update()
     time.sleep(.1)
-    for turtle_index in range(len(turtle_list) - 1,0,-1):
-        new_x = turtle_list[turtle_index -1].xcor()
-        new_y = turtle_list[turtle_index -1].ycor()
-        turtle_list[turtle_index].goto(x=new_x, y=new_y)
-    turtle_list[0].fd(20)
-    
-        
+
+    snake.move()
+
 
 screen.exitonclick()
---
+++ snake_game/snake.py
@@ -0,0 +1,27 @@
+from turtle import Screen, Turtle, color
+import time
+
+
+x_position = 0
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self): 
+        global x_position
+        for turtle in range(0,3):
+            x_position -= 20
+            new_turtle = Turtle()
+            new_turtle.penup()
+            new_turtle.color("white")
+            new_turtle.shape("square")
+            new_turtle.goto(x=x_position, y=0)
+            self.segments.append(new_turtle)
+    
+    def move(self):
+        for segment in range(len(self.segments) - 1,0,-1):
+            new_x = self.segments[segment -1].xcor()
+            new_y = self.segments[segment -1].ycor()
+            self.segments[segment].goto(x=new_x, y=new_y)
+        self.segments[0].fd(20)
