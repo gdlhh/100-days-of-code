9af15da1470064450612d501d5d9b4a7ac6f4215
gdlhh <han_ger05@hotmail.com>
2022-09-22 03:33:26 +0800

    snake game is now able to initialise and move

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -25,3 +25,19 @@ class Snake:
             new_y = self.segments[segment -1].ycor()
             self.segments[segment].goto(x=new_x, y=new_y)
         self.segments[0].fd(20)
+
+    def up(self):
+        if self.segments[0].heading() != 270:
+            self.segments[0].setheading(90)
+
+    def down(self):
+        if self.segments[0].heading() != 90:
+            self.segments[0].setheading(270)
+
+    def left(self):
+        if self.segments[0].heading() != 0:
+            self.segments[0].setheading(180)
+
+    def right(self):
+        if self.segments[0].heading() != 180:
+            self.segments[0].setheading(0)
